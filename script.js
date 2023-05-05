31adf003b5f9800ef11ed760d713f8065fe447d4
liban13130 <nabil-13130@hotmail.fr>
2023-05-05 21:24:53 +0200

    fini

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 // http://api.airvisual.com/v2/nearest_city?key={800f08d3-c18c-4abe-b706-f0c75f7470c9}
 
-const loader = document.querySelector('.loader-container');
 const errorInformation = document.querySelector('.error-info');
+const loader = document.querySelector('.loader-container');
 
 async function getWeatherData(){
-    try {  // Try = essaie ce code et si il y'a une erreur alors CATCH (ATTRAPE LA)
-        const response = await fetch("http://api.airvisual.com/v2/nearest_city?key=800f08d3-c18c-4abe-b706-f0c75f7470c9") //On attend le resultat de fetch
-        //La methode fetch va effectuer une requete http, on recupere les donnés et on attend la response juste en bas
-
-        const responseData = await response.json() // On attend le resultat de json car sinon on ne peut pas lire les données. 
-        // Pour pouvoir lire la reponse json on utilise await aussi pour lire le JSON et pour UTILISER LES DONNES que l'on souhaite
-        // Ensuite on utilisera une fonction qui affichera les données que l'on a trié dans un objet
-
-        const triData = {
-            city: responseData.data.city,
-            country: responseData.data.country,
-            iconId: responseData.data.current.weather.ic,
-            temperature: responseData.data.current.weather.tp
-        }
-        console.log(triData);
-    } catch (error) {
+    try {
+        const response = await fetch("http://api.airvisual.com/v2/nearest_city?key=800f08d3-c18c-4abe-b706-f0c75f7470c9").catch(() => {
+            throw new Error("Problème de connexion...")
+        })
+        console.log(response);
         
+        if(!response.ok){
+            throw new Error(`Erreur ${response.status}, ${response.statusText}`)
+        }
+
+        const dataResponse = await response.json()
+        const triData = {
+            city: dataResponse.data.city,
+            country: dataResponse.data.country,
+            temperature: dataResponse.data.current.weather.tp,
+            iconId: dataResponse.data.current.weather.ic
+        }
+
+        populateUI(triData)
+
+    } catch (error) {
+        loader.classList.remove("active")
+        errorInformation.textContent = error.message
     }
 }
 
 getWeatherData()
+// document.querySelector('.get-data').addEventListener('click', getWeatherData)
+
+
+const city        = document.querySelector('.city-name');
+const country     = document.querySelector('.country-name');
+const temperature = document.querySelector('.temperature');
+const iconId      = document.querySelector('.info-icon');
+
+function populateUI(data){
+    city.textContent = data.city
+    country.textContent = data.country
+    temperature.textContent = `${data.temperature}°`
+    iconId.src = `ressources/icons/${data.iconId}.svg`
+    iconId.style.width = "150px"
+    loader.classList.remove("active")
+}
